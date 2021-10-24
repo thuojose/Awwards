222342643098822746e6b97cdfc5d4bcb9beb7b0
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-24 23:33:57 +0300

    added function to get profile

--- awewards/views.py
+++ awewards/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.http import Http404
 from django.contrib.auth.views import LogoutView
 
 
 # Create your views here.
+
 
 def register(request):
     if request.method == 'POST':
@@ -15,8 +19,18 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', locals())
 
+
 @login_required(login_url='/accounts/login')
 def home(request):
     current_user = request.user
     all_projects = Projects.objects.all()
     return render(request, 'index.html', locals())
+
+
+@login_required(login_url='/accounts/login')
+def project(request, project_id):
+    try:
+        project = Projects.objects.get(id=project_id)
+    except Projects.DoesNotExist:
+        raise Http404()
+    return render(request, "project.html", locals())
