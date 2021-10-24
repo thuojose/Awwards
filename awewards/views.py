edd4009e10dd52bf3d57148084e17bef4a67a69d
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-24 22:25:31 +0300

    added fuction to get projects

--- awewards/views.py
+++ awewards/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+
 
 # Create your views here.
 
@@ -12,3 +15,8 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', locals())
 
+@login_required(login_url='/accounts/login')
+def home(request):
+    current_user = request.user
+    all_projects = Projects.objects.all()
+    return render(request, 'index.html', locals())
