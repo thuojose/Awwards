62a8675ee8d9d942e5f12a50b15d1ea23c558dbd
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-24 23:37:10 +0300

    added function to get profile

--- awewards/views.py
+++ awewards/views.py
@@ -34,3 +34,18 @@ def project(request, project_id):
     except Projects.DoesNotExist:
         raise Http404()
     return render(request, "project.html", locals())
+
+
+@login_required(login_url='/accounts/login')
+def profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UploadForm()
+        my_projects = Projects.objects.filter(uploader=current_user)
+        my_profile = Profile.objects.get(user_id=current_user)
+    return render(request, 'profile.html', locals())
