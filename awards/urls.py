c21ddb2da53bf69e64653be841aac816a539448c
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-24 22:46:50 +0300

    added url for different components

--- awards/urls.py
+++ awards/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from awewards import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('awewards.urls')),
+    path('accounts/logout/', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
+    path('logout/', views.logout, name='logout'),
 ]
