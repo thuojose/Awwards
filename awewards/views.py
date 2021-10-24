40b2070556a95fd9b53c6050be7e893dd1c0f5f2
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-24 21:59:28 +0300

    added function for user registration

--- awewards/views.py
+++ awewards/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', locals())
+
